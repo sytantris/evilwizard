af8c354e5ed56a96fb0611d58db0796353da7cbb
Sytantris Dyat <sytantris@gmail.com>
2016-04-11 16:50:22 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+Evil Wizard Elixirs
+=================
+
+
+Setting up the dev environment
+------------------------------
+
+Bundle install:
+
+    bundle install --without production
+
+Initializing the database:
+
+    bundle exec rake db:migrate
+    bundle exec rake db:seed
+
+Start the server:
+
+    bundle exec rails server
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvilWizard::Application.config.secret_token = '9ce92f60cfdaac1b733ce90ff4c204916461a58fa48a67565755ef0797687a631fe1aab75caa5b397dc0ccf53a3fdd776eadeb688c7fc7005134bfe903e4c5dc'
